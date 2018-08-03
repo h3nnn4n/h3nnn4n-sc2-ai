3203385d2ac25cabd6f37d07aa1a2aa1470c3dc3
h3nnn4n <uber.renan@gmail.com>
2018-08-03 20:55:06 -0300

    Minor improvements for micro manager

--- src/spatial.py
+++ src/spatial.py
@@ -13,7 +13,7 @@ class UnitMicro:
     def __init__(self, tag, unit_type, bot, scouting=False, defending=False):
         self.tag = tag
         self.unit_type = unit_type
-        self.current_target = None
+        self.target = None
         self.scouting = scouting
         self.defending = defending
         self.attacking = not scouting and not defending
@@ -26,28 +26,35 @@ class UnitMicro:
         self.can_attack_air = self.unit.can_attack_air
         self.can_attack_ground = self.unit.can_attack_ground
 
-    async def update(self):
+    def update_target(self):
+        if self.bot.units.find_by_tag(self.target) is None:
+            target = None
+
+            if self.bot.known_enemy_structures().amount == 0:
+                near_units = self.bot.known_enemy_units().closer_than(self.ground_range, self.unit)
+                if near_units.exists:
+                    target = near_units.closest_to(self.unit)
+
+            if self.bot.known_enemy_units().amount > 0:
+                near_structures = self.bot.known_enemy_structures().closer_than(self.ground_range, self.unit)
+                if near_structures.exists:
+                    target = near_structures.closest_to(self.unit)
+
+            self.target = target
+
+    def get_target(self):
+        return self.target
+
+    def alive(self):
         self.unit = self.bot.units.find_by_tag(self.tag)
-        if not self.unit is not None:
-            return
+        return True if self.unit is not None else False
 
-        near_ground_units = self.bot.known_enemy_units().closer_than(self.ground_range, self.unit)
-        near_air_units = self.bot.known_enemy_units().closer_than(self.air_range, self.unit)
-        near_structures = self.bot.known_enemy_structures().closer_than(self.ground_range, self.unit)
+    async def update(self):
+        if self.alive():
+            self.update_target()
 
-        target = None
-
-        if near_air_units.exists:
-            target = near_air_units.closest_to(self.unit)
-        elif near_ground_units.exists:
-            target = near_ground_units.closest_to(self.unit)
-        elif near_structures.exists:
-            target = near_structures.closest_to(self.unit)
-
-        if target is None:
-            return
-
-        await self.bot.do(self.unit.attack(target))
+            if self.target is not None:
+                await self.bot.do(self.unit.attack(self.target))
 
 
 class EventManager:

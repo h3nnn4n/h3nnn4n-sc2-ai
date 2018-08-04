4c605c9b98d8a73a76928e261d50ff4ffd8445a5
h3nnn4n <uber.renan@gmail.com>
2018-08-04 19:13:05 -0300

    Made SpatialLlama ladder ready

---
+++ SpatialLlama/run.py
@@ -0,0 +1,19 @@
+import sc2, sys
+from __init__ import run_ladder_game
+from sc2 import Race, Difficulty
+from sc2.player import Bot, Computer
+from SpatialLlama import SpatialLlama
+
+
+if __name__ == '__main__':
+    bot = Bot(Race.Protoss, SpatialLlama())
+
+    if "--LadderServer" in sys.argv:
+        print("Starting ladder game...")
+        run_ladder_game(bot)
+    else:
+        print("Starting local game...")
+        sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+            bot,
+            Computer(Race.Protoss, Difficulty.VeryHard)
+        ], realtime=True)

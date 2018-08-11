e3bb456dbe1bc47b7c933ba25cd4e589cae0bee9
h3nnn4n <uber.renan@gmail.com>
2018-08-11 20:27:48 -0300

    Added more maps

--- TapiocaBot/TapiocaBot/run.py
+++ TapiocaBot/TapiocaBot/run.py
@@ -6,6 +6,27 @@ from sc2.player import Bot, Computer
 from TapiocaBot import TapiocaBot
 
 
+maps = [
+    '(2)16-BitLE',
+    '(2)CatalystLE',
+    '(2)LostandFoundLE',
+    '(4)DarknessSanctuaryLE',
+    '(2)AcidPlantLE',
+    '(2)DreamcatcherLE',
+    '(2)RedshiftLE',
+    'AbyssalReefLE',
+    'AbiogenesisLE',
+    'BackwaterLE',
+    'CatalystLE',
+    'NeonVioletSquareLE',
+    'AcidPlantLE',
+    'BlackpinkLE',
+    'EastwatchLE',
+]
+
+selected_map = maps[2]
+
+
 if __name__ == '__main__':
     if "--LadderServer" in sys.argv:
         bot = Bot(Race.Protoss, TapiocaBot())
@@ -14,7 +35,7 @@ if __name__ == '__main__':
     else:
         print("Starting local game...")
         bot = Bot(Race.Protoss, TapiocaBot(verbose=True, visual_debug=True))
-        sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+        sc2.run_game(sc2.maps.get(selected_map), [
             bot,
             Computer(Race.Protoss, Difficulty.VeryHard)
         ], realtime=False)

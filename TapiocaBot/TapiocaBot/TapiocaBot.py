91167c92cb2ba0f3fbc72843a1adbbcf60234406
h3nnn4n <uber.renan@gmail.com>
2018-08-18 23:39:26 -0300

    Fixed typo

--- TapiocaBot/TapiocaBot/TapiocaBot.py
+++ TapiocaBot/TapiocaBot/TapiocaBot.py
@@ -77,7 +77,7 @@ class TapiocaBot(sc2.BotAI):
                 print('\n------------------------\n')
                 print('%8.2f %3d Rise and Shine' % (self.time, self.supply_used))
 
-            await self.chat_send("Cry 'havoc', and let slips the Tapiocas of war!")
+            await self.chat_send("Cry 'havoc', and let slip the Tapiocas of war!")
 
             return
 

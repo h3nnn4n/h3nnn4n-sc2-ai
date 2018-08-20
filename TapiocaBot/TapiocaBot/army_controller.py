9a8ec07ef5268efce41e75461f47f9e2a39e48e5
h3nnn4n <uber.renan@gmail.com>
2018-08-19 21:16:34 -0300

    Moved Stalker Logic to its own class

--- TapiocaBot/TapiocaBot/army_controller.py
+++ TapiocaBot/TapiocaBot/army_controller.py
@@ -2,6 +2,8 @@ import random
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
 
+from stalker_controller import StalkerController
+
 
 class ArmyController:
     def __init__(self, bot=None, verbose=False):
@@ -73,6 +75,8 @@ class ArmyController:
         self.attack_target = None
         self.defense_target = None
         self.first_attack = True
+
+        self.stalker_controller = StalkerController(bot=self.bot, verbose=self.verbose)
 
     def init(self):
         self.map_center = self.bot.game_info.map_center
@@ -231,118 +235,7 @@ class ArmyController:
                 await self.bot.do(unit.attack(self.attack_target.position))
 
     async def stalker_micro(self, unit_tag):
-        # self.bot._client.game_step = 2
-        visual_debug = False
-        font_size = 14
-
-        unit = self.bot.units.find_by_tag(unit_tag)
-
-        if self.bot.known_enemy_units.exclude_type(self.units_to_ignore_attacking).amount == 0:
-            if unit.is_idle:
-                self.attack_target = self.get_something_to_attack()
-                await self.bot.do(unit.attack(self.attack_target.position))
-            return
-
-        all_enemy_units = self.bot.known_enemy_units.exclude_type(self.units_to_ignore_attacking)
-        enemy_units = all_enemy_units.not_structure
-
-        if enemy_units.exists:
-            closest_unit = enemy_units.closest_to(unit)
-        else:
-            closest_unit = all_enemy_units.closest_to(unit)
-
-        distance_to_closest_unit = unit.distance_to(closest_unit) - unit.radius / 2 - closest_unit.radius / 2 + 0.1
-
-        step_back_position = unit.position.towards(closest_unit.position, -2)
-
-        our_range = unit.ground_range + unit.radius
-        enemy_range = closest_unit.ground_range + closest_unit.radius
-
-        if visual_debug:
-            self.bot._client.debug_sphere_out(unit, r=our_range, color=(0, 255, 255))
-            self.bot._client.debug_sphere_out(closest_unit, r=enemy_range, color=(255, 0, 0))
-            self.bot._client.debug_line_out(unit, closest_unit, color=(127, 127, 255))
-            self.bot._client.debug_line_out(unit, step_back_position, color=(127, 0, 255))
-
-        if unit.is_idle:
-            self.attack_target = self.get_something_to_attack()
-            await self.bot.do(unit.attack(self.attack_target.position))
-
-            if visual_debug:
-                self.bot._client.debug_text_world('cant see', pos=unit.position3d, size=font_size)
-            return
-
-        if our_range > enemy_range:  # Stalker outrange target
-            if our_range < distance_to_closest_unit:  # But we are not in range
-                if visual_debug:
-                    self.bot._client.debug_text_world('too far', pos=unit.position3d, size=font_size)
-                if unit.weapon_cooldown > 0:  # If weapon is on cool down we right click the unit
-                    await self.bot.do(unit.attack(closest_unit))
-                else:  # Else we right click the unit too
-                    await self.bot.do(unit.attack(closest_unit))
-            elif enemy_range + 0.1 < distance_to_closest_unit:  # They are in our range but we arent in theirs
-                if closest_unit.is_structure:  # Get closer to structures
-                    if visual_debug:
-                        self.bot._client.debug_text_world('atk structure', pos=unit.position3d, size=font_size)
-                    if unit.weapon_cooldown > 0 and distance_to_closest_unit > closest_unit.radius + 1:
-                        advance_position = unit.position.towards(closest_unit.position, distance=1)
-                        await self.bot.do(unit.move(advance_position))
-                    else:
-                        await self.bot.do(unit.attack(closest_unit))
-                else:
-                    if our_range - distance_to_closest_unit > 0.75:
-                        step_back_position = unit.position.towards(closest_unit.position, -2)
-                        if visual_debug:
-                            self.bot._client.debug_text_world('closeish', pos=unit.position3d, size=font_size)
-                        await self.bot.do(unit.move(step_back_position))
-                    else:
-                        if visual_debug:
-                            self.bot._client.debug_text_world('ideal', pos=unit.position3d, size=font_size)
-                        await self.bot.do(unit.attack(closest_unit))
-            else:  # We are in their range but we can out range them
-                # if unit.weapon_cooldown == 0:  # shoot first
-                #     await self.bot.do(unit.attack(closest_unit))
-                # else:
-                #     distance = enemy_range - distance_to_closest_unit
-                #     step_back_position = unit.position.towards(closest_unit.position, -distance)
-
-                #     await self.bot.do(unit.move(step_back_position))
-                # distance = enemy_range - distance_to_closest_unit
-
-                abilities = await self.bot.get_available_abilities(unit)
-
-                if unit.shield_percentage < 0.1 and AbilityId.EFFECT_BLINK_STALKER in abilities:
-                    blink_back_position = unit.position.towards(closest_unit.position, -8)
-                    await self.bot.do(unit(AbilityId.EFFECT_BLINK_STALKER, blink_back_position))
-                else:
-                    step_back_position = unit.position.towards(closest_unit.position, -1)
-                    await self.bot.do(unit.move(step_back_position))
-
-                if visual_debug:
-                    self.bot._client.debug_text_world('too close', pos=unit.position3d, size=font_size)
-        else:  # we either have the same range or we have less range
-            abilities = await self.bot.get_available_abilities(unit)
-
-            if unit.shield_percentage < 0.1 and AbilityId.EFFECT_BLINK_STALKER in abilities:
-                blink_back_position = unit.position.towards(closest_unit.position, -8)
-                await self.bot.do(unit(AbilityId.EFFECT_BLINK_STALKER, blink_back_position))
-            else:
-                if enemy_range >= distance_to_closest_unit:
-                    if unit.shield_percentage < 0.1 and AbilityId.EFFECT_BLINK_STALKER in abilities:
-                        blink_back_position = unit.position.towards(closest_unit.position, -8)
-                        await self.bot.do(unit(AbilityId.EFFECT_BLINK_STALKER, blink_back_position))
-                        if visual_debug:
-                            self.bot._client.debug_text_world('blink', pos=unit.position3d, size=font_size)
-                    else:
-                        if unit.weapon_cooldown == 0:
-                            await self.bot.do(unit.attack(closest_unit))
-                            if visual_debug:
-                                self.bot._client.debug_text_world('YOLO', pos=unit.position3d, size=font_size)
-                        else:
-                            step_back_position = unit.position.towards(closest_unit.position, -1)
-                            await self.bot.do(unit.move(step_back_position))
-                            if visual_debug:
-                                self.bot._client.debug_text_world('back', pos=unit.position3d, size=font_size)
+        await self.stalker_controller.control(unit_tag)
 
     async def defend(self, unit_tag):
         unit = self.bot.units.find_by_tag(unit_tag)
